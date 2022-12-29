d4f157fa059a9bce059f267f181544e2e0135000
Taraslysak <lysak.ipr@gmail.com>
2022-12-29 18:26:57 +0200

    sample api env + config fix

--- api/app/config.py
+++ api/app/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str = "redis"
     REDIS_PORT: int = 6379
     REDIS_PASS: str = "dummy_pass"
-    JWT_SECRET: str
+    JWT_SECRET: str = "dummy_secret"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     class Config:
