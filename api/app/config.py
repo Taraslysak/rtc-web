d7b4dfb20932a8f8846df47fedabc54a21778e12
Taraslysak <lysak.ipr@gmail.com>
2023-01-09 11:51:47 +0200

    postgres DB setup file

--- api/app/config.py
+++ api/app/config.py
@@ -3,10 +3,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     SAMPLE_ENV_VAR: str = "<None>"
-    REDIS_HOST: str = "redis"
-    REDIS_PORT: int = 6379
-    REDIS_PASS: str = "dummy_pass"
-    JWT_SECRET: str = "dummy_secret"
+    DATABASE_URI: str = "UNKNOWN_HOST"
+    DEV_DATABASE_URI: str = "UNKNOWN_DEV_HOST"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     WS_TOKEN_EXPIRE_MINUTES: int = 1
 
