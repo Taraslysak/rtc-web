3f8ebe7e4ec652bcccab2d92e864d5e0141af063
Taraslysak <lysak.ipr@gmail.com>
2023-01-15 16:21:14 +0200

    added constant

--- front/src/navigation/router.tsx
+++ front/src/navigation/router.tsx
@@ -1,20 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Auth } from "../pages/Auth";
-import {Intro} from "../pages/Intro";
+import { Intro } from "../pages/Intro";
 import { Main } from "../pages/Main";
+import { ROUTE_PATH } from "../strings";
 
 export const router = createBrowserRouter([
-
-    {
-      path: "/auth",
-      element: <Auth/>,
-    },
-    {
-      path: "/main",
-      element: <Main/>,
-    },
-    {
-      path: "/",
-      element: <Intro/>,
-    },
-  ]);
+  {
+    path: ROUTE_PATH.AUTH,
+    element: <Auth />,
+  },
+  {
+    path: ROUTE_PATH.MAIN,
+    element: <Main />,
+  },
+  {
+    path: ROUTE_PATH.INTRO,
+    element: <Intro />,
+  },
+]);
--- front/src/pages/Intro/Intro.tsx
+++ front/src/pages/Intro/Intro.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { LOCAL_STORAGE_KEY, ROUTE_PATH } from "../../strings";
 
 export function Intro() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = localStorage.getItem("AUTH_TOKEN");
-    navigate(token ? "main" : "auth");
+    const token = localStorage.getItem(LOCAL_STORAGE_KEY.AUTH_TOKEN);
+    navigate(token ? ROUTE_PATH.MAIN : ROUTE_PATH.AUTH);
   }, []);
 
   return <div>Intro</div>;
